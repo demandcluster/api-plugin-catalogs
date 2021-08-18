bc4bca805cdd1d6aed6bfe7be4f3283ed7347bb0
DemandCluster <devops@demandcluster.com>
2021-08-18 20:43:29 +0200

    chore: .yalc cleanup

--- .yalc/@reactioncommerce/api-plugin-catalogs/src/resolvers/Query/index.js
+++
@@ -1,9 +0,0 @@
-import catalogItems from "./catalogItems.js";
-import catalogItemProduct from "./catalogItemProduct.js";
-import vendors from "./vendors.js";
-
-export default {
-  catalogItems,
-  catalogItemProduct,
-  vendors
-};
